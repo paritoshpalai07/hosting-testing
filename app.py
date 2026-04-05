60450d02e91616cac1e1cc327225e94e483ed43b
Paritosh Palai <paritoshpalai07@gmail.com>
2026-04-05 18:34:41 +0530

    sixth commit

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# Vercel looks for this
+def handler(request, context):
+    return app(request.environ, context.start_response)
